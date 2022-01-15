4536a986781b781bf80f6b5e2141f44755df1c4d
Sophie Oliver <sophieoliver@Sophies-Machine.local>
2022-01-15 14:21:55 -0800

    axios get from client to server - display message in browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api")
+      .then((res) => { setData(() => res.data.message)})
+      .catch((er) => console.log(er))
+    }, [])
+
   return (
     <div className="App">
-      
+      <p>{data ? data : "Loading..."}</p>
     </div>
   );
 }
